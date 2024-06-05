c02cdeea6ca0cc6f66efa1fc31db449f3e638f29
wazza <cryptowazza@protonmail.com>
2024-06-05 17:19:26 +0800

    allow chaining on StepDataSchemaBroadcastooor

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -14,15 +14,17 @@ export class StepDataSchemaBroadcastooor {
     this.socket = this.manager.socket("/data_schema");
   }
 
-  onConnect(listener: () => void | Promise<void>) {
+  onConnect(listener: () => void | Promise<void>): this {
     this.socket.on("connect", listener);
+    return this;
   }
 
-  onDisconnect(listener: () => void | Promise<void>) {
+  onDisconnect(listener: () => void | Promise<void>): this {
     this.socket.on("disconnect", listener);
+    return this;
   }
 
-  subscribe(topic: string, filterId?: string, filterExpression?: string) {
+  subscribe(topic: string, filterId?: string, filterExpression?: string): this {
     if (!!filterId !== !!filterExpression) {
       throw new Error("filterId and filterExpression must both be set or both be undefined");
     } else if (filterId && filterExpression) {
@@ -30,26 +32,32 @@ export class StepDataSchemaBroadcastooor {
     } else {
       this.socket.emit("subscribe", { topic });
     }
+    return this;
   }
-  
-  unsubscribe(topic: string, filterId?: string) {
+
+  unsubscribe(topic: string, filterId?: string): this {
     this.socket.emit("unsubscribe", { topic, filter_id: filterId });
+    return this;
   }
 
-  onReceivedSchema(listener: (message: SchemaMessage) => void | Promise<void>) {
+  onReceivedSchema(listener: (message: SchemaMessage) => void | Promise<void>): this {
     this.socket.on("receivedSchema", listener);
+    return this;
   }
 
-  onSubscribed(listener: (topic: string) => void | Promise<void>) {
+  onSubscribed(listener: (topic: string) => void | Promise<void>): this {
     this.socket.on("subscribed", listener);
+    return this;
   }
 
-  onUnsubscribed(listener: (topic: string) => void | Promise<void>) {
+  onUnsubscribed(listener: (topic: string) => void | Promise<void>): this {
     this.socket.on("unsubscribed", listener);
+    return this;
   }
 
-  onServerError(listener: (error: string) => void | Promise<void>) {
+  onServerError(listener: (error: string) => void | Promise<void>): this {
     this.socket.on("serverError", listener);
+    return this;
   }
 
   close() {
